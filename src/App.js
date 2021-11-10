ae70f9c2e7802975232b6e7bb2adc53a57c7522a
Weili Xu <weili.xu@pnnl.gov>
2021-11-10 11:45:15 -0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Testing GITHUB PAGES with react and github actions</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Component} from 'react'
+import {Row, Col} from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    //this.state = {
+    //  selectedSample:'',
+    //  selectedSampleUrl: ''
+    //}
+  }
+
+  //
+  render(){
+    return (
+      <Container>
+        <div className = "container">
+          <Jumbotron>
+            <h1 className='mb-3'>Placeholder data name</h1>
+            <form>
+              <Row className = 'align-items-center mb-3'>
+                <Col md={6}>
+                  <h1>Place holder for first dataset</h1>
+                </Col>
+                <Col md={6}>
+                  <h1>Place holder for second dataset</h1>
+                </Col>
+              </Row>
+            </form>
+          </Jumbotron>
+        </div>
+      </Container>
+    );
+  }
 }
+
 
 export default App;
